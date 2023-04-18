290ba15f3767729f94c23372b097bae0e7de32b2
Anton <email.b.anton@gmail.com>
2023-04-19 00:20:56 +0300

    HW

---
+++ Lesson1/Practical/Lesson1_practical4/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("How are you?");
+        Scanner sc = new Scanner(System.in);
+        String a;
+        a = sc.next();
+        System.out.println("You answer is:");
+        System.out.println(a);
+    }
+}
