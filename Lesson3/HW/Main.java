305b0a92f65a5f94b094d027b62452d396110f97
Anton <email.b.anton@gmail.com>
2023-04-23 15:27:21 +0300

    HW1L3

--- Lesson3/HW/Main.java
+++ Lesson3/HW/Main.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 import static java.lang.Double.sum;
 
 public class Main {
-    public double numberA;
-    public double numberB;
-    private static int count = 0;
-    public Main() {
-        count++;
-    }
-    public static double getTotal(double numberA, double numberB) {
-        return sum(numberA, numberB);
-    }
+    public double sideA;
+    public double sideB;
+    public double sideC;
 
-    public static double getAverage(double numberA, double numberB) {
-        return sum(numberA, numberB) / count;
+    public Main() {
+
+    }
+    public static double getArea(double sideA, double sideB, double sideC) {
+        double p = (double) (sideA + sideB + sideC) / 2;
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
     }
 
     public static double getNumber(Scanner scanner, String prompt) {
@@ -25,15 +23,17 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        double numberA = getNumber(scanner, "Enter the number A: ");
-        count++;
-        double numberB = getNumber(scanner, "Enter the number B: ");
-        count++;
-        double sum = getTotal(numberA, numberB);
-        double avg = getAverage(numberA, numberB);
+        double sideA = getNumber(scanner, "Enter the side A: ");
 
-        System.out.println("The sum of the numbers is: " + sum);
-        System.out.println("The average of the numbers is: " + avg);
+        double sideB = getNumber(scanner, "Enter the side B: ");
+
+        double sideC = getNumber(scanner, "Enter the side B: ");
+
+        double area = getArea(sideA, sideB, sideC);
+
+
+        System.out.println("The area of a triangle is: " + area);
+
 
         scanner.close();
     }
