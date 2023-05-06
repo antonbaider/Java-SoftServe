a955d7686446e03e6d4dee0b3ac83dfb17197954
antonbaider <89017124+antonbaider@users.noreply.github.com>
2023-05-06 12:12:41 +0300

    Create Month.java

---
+++ Lesson5/Homework/1/Month.java
@@ -0,0 +1,27 @@
+package Month;
+
+public class Month {
+
+     String name;
+      int days;
+
+    public Month(String name, int days) {
+        this.name = name;
+        this.days = days;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+}
+
+
+
+
+
