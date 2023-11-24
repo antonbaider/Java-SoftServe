cb90cdf047b1dd377831c41090c9c2708367cff7
Anton <89017124+antonbaider@users.noreply.github.com>
2023-11-24 17:05:09 +0100

    Update Employeer2.java

--- Lesson7/Homework/1/Employeer2.java
+++ Lesson7/Homework/1/Employeer2.java
@@ -2,49 +2,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 abstract class Employee {
-    public Employee(String employeeId,String name) {
+    String employeeId;
+    String name;
+    public Employee(String employeeId, String name) {
         this.employeeId = employeeId;
         this.name = name;
     }
-    String employeeId;
-    String name;
+
     interface Payment {
-       public double calculatePay();
+        public double calculatePay();
     }
 }
+
 class SalariedEmployee extends Employee implements Employee.Payment {
-    private String socialSecurityNumber;
-private double monthlyPay;
+    private final String socialSecurityNumber;
+    private double monthlyPay;
+
     public SalariedEmployee(String employeeId, String name, String socialSecurityNumber) {
-        super(employeeId,name);
+        super(employeeId, name);
         this.socialSecurityNumber = socialSecurityNumber;
     }
+
     public String getSocialSecurityNumber() {
         return socialSecurityNumber;
     }
+
     @Override
     public double calculatePay() {
         return monthlyPay;
     }
 }
+
 class ContractEmployee extends Employee implements Employee.Payment {
-    private String federalTaxIdMember;
+    private final String federalTaxIdMember;
     private double monthlyPay;
+
     public ContractEmployee(String employeeId, String name, int federalTaxIdMember) {
-        super(employeeId,name);
+        super(employeeId, name);
         this.federalTaxIdMember = String.valueOf(federalTaxIdMember);
     }
+
     public String federalTaxIdMember() {
         return federalTaxIdMember;
     }
+
     @Override
     public double calculatePay() {
         return monthlyPay;
     }
 }
+
 class HourlyPaidWorker extends SalariedEmployee {
-    private double hourlyRate;
-    private double hoursWorked;
+    private final double hourlyRate;
+    private final double hoursWorked;
 
     public HourlyPaidWorker(String employeeId, String name, String socialSecurityNumber, double hourlyRate, double hoursWorked) {
         super(employeeId, name, socialSecurityNumber);
@@ -69,13 +79,15 @@ class HourlyPaidWorker extends SalariedEmployee {
     public double getHoursWorked() {
         return hoursWorked;
     }
+
     @Override
     public double calculatePay() {
         return hourlyRate * hoursWorked;
     }
 }
+
 class FixedPaidWorker extends ContractEmployee {
-    private double fixedMonthlyPayment;
+    private final double fixedMonthlyPayment;
 
     public FixedPaidWorker(String employeeId, String federalTaxId, String number, double fixedMonthlyPayment) {
         super(employeeId, federalTaxId, 0);
@@ -100,12 +112,13 @@ class FixedPaidWorker extends ContractEmployee {
         return fixedMonthlyPayment;
     }
 }
+
 public class Employee2 {
     public static void main(String[] args) {
-         List<Employee> employees = new ArrayList<>();
+        List<Employee> employees = new ArrayList<>();
         employees.add(new HourlyPaidWorker("111", "STEPAN", "1112", 33, 87));
         employees.add(new HourlyPaidWorker("222", "GALYNA", "2221", 23, 40));
-        employees.add(new FixedPaidWorker("333", "PETRO", "1212", 13, 22));
-            System.out.println(employees);
+        employees.add(new FixedPaidWorker("333", "PETRO", "1212", 122));
+        System.out.println(employees);
     }
 }
